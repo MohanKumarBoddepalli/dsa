9b2b31ad40d9194bfa8fb1ac4b43e27f29026207
Mohan Kumar Boddepalli <126196778+MohanKumarBoddepalli@users.noreply.github.com>
2024-03-17 23:31:43 +0530

    day3

---
+++ src/2-Array/easy/anagrom.js
@@ -0,0 +1,56 @@
+/**
+ *    https://leetcode.com/problems/valid-anagram/description/
+ *    242. Valid Anagram
+ *    Solved
+ *    Easy
+ *    Topics
+ *    Companies
+ *    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *    An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ */
+
+/**
+ *    Example 1:
+ *    Input: s = "anagram", t = "nagaram"
+ *    Output: true
+ */
+
+/**
+ *    Example 2:
+ *    Input: s = "rat", t = "car"
+ *    Output: false
+ *    Constraints:
+ *    1 <= s.length, t.length <= 5 * 104
+ *    s and t consist of lowercase English letters.
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  let inputString = new Map();
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (inputString[s[i]]) {
+      inputString[s[i]] += 1;
+    } else {
+      inputString[s[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (inputString[t[i]] > 0) {
+      inputString[t[i]] -= 1;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+isAnagram("anagram", "nagaram");
---
+++ src/stack/easy/valid-parentheses.js
@@ -0,0 +1,55 @@
+
+// https://leetcode.com/problems/valid-parentheses/
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+/**
+ * Example 1:
+ * Input: s = "()"
+ * Output: true
+ */
+
+/**
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ */
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+ 
+
+/** Constraints: */
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (s.length <= 1) {
+      return false
+  }
+  const stack = [];
+  const keys = {
+      '{': '}',
+      '(': ')',
+      '[': ']',
+  }
+  for (let i = 0; i < s.length; i++) {
+      if (keys[s[i]]) {
+          stack.push(keys[s[i]]);
+      }
+      else if (stack.pop() !== s[i]) {
+          return false
+      }
+  }
+  return stack.length === 0;
+};
